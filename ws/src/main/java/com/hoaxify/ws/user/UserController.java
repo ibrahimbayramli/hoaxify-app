907d6da174ff4c692951599ab6762e10467f49de
İbrahim BAYRAMLI <ibrahim.bayramli@atmosware.turkcell.com.tr>
2023-04-07 01:58:51 +0300

    11 - Spring Rest Controller (Spring)

---
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -0,0 +1,16 @@
+package com.hoaxify.ws.user;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class UserController {
+
+	@PostMapping("/api/1.0/users")
+	public void createUser(@RequestBody User user){
+		log.info(user.toString());
+	}
+}
