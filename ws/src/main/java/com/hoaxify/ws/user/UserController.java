c807e8c692e39263a7ce5e7d17ea82351ea83754
İbrahim BAYRAMLI <ibrahim.bayramli@atmosware.turkcell.com.tr>
2023-04-08 16:42:48 +0300

    17 - Refactor: Spring Projesi (Spring)

--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,10 +1,13 @@
 package com.hoaxify.ws.user;
 
 
+import com.hoaxify.ws.shared.GenericResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,11 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
 	@Autowired
-	UserRepository userRepository;
+	UserService userService;
 
 	@PostMapping("/api/1.0/users")
-	public void createUser(@RequestBody User user){
-		userRepository.save(user);
+	@ResponseStatus(HttpStatus.CREATED)
+	public GenericResponse createUser(@RequestBody User user){
+		userService.save(user);
 		log.info(user.toString());
+
+		return new GenericResponse("User Created");
 	}
 }
