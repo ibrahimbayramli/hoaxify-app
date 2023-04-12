e219c82608a88c5da210906424a3e11c46e42886
İbrahim BAYRAMLI <ibrahimbayramli1902@gmail.com>
2023-04-12 03:28:06 +0300

    48 - Kimlik Doğrulama Hata Cevabı (Spring)

--- ws/src/main/java/com/hoaxify/ws/auth/AuthController.java
+++ ws/src/main/java/com/hoaxify/ws/auth/AuthController.java
@@ -7,13 +7,13 @@ import com.hoaxify.ws.user.User;
 import com.hoaxify.ws.user.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.util.Base64;
@@ -24,40 +24,22 @@ import java.util.Map;
 @Slf4j
 public class AuthController {
 
-	@Autowired
-	UserRepository userRepository;
+    @Autowired
+    UserRepository userRepository;
 
-	PasswordEncoder passwordEncoder=new BCryptPasswordEncoder();
 
-	@JsonView(Views.Base.class)
-	@PostMapping("/api/1.0/auth")
-	ResponseEntity<?> handleAuthentication(@RequestHeader(name = "Authorization",required = false) String authorization){
-		if(authorization==null){
-			ApiError error=new ApiError(401,"Unauthorized request","/api/1.0/auth");
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
-		}
-		String base64encoded=authorization.split("Basic ")[1];
 
-		String decode=new String(Base64.getDecoder().decode(base64encoded));
-		String[] parts=decode.split(":");
-		String username=parts[0];
-		String password=parts[1];
-		User inDB=userRepository.findByUsername(username);
-		if(inDB==null){
-			ApiError error=new ApiError(401,"Unauthorized request","/api/1.0/auth");
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
-		}
-		String hashedPassword=inDB.getPassword();
-		if (!passwordEncoder.matches(password,hashedPassword)){
-			ApiError error=new ApiError(401,"Unauthorized request","/api/1.0/auth");
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
-		}
+    @JsonView(Views.Base.class)
+    @PostMapping("/api/1.0/auth")
+    ResponseEntity<?> handleAuthentication(@RequestHeader(name = "Authorization") String authorization) {
 
-		Map<String,String> responseBody=new HashMap<>();
-		responseBody.put("username",inDB.getUsername());
-		responseBody.put("displayName",inDB.getDisplayName());
-		responseBody.put("image",inDB.getImage());
+        String base64encoded = authorization.split("Basic ")[1];
+        String decode = new String(Base64.getDecoder().decode(base64encoded));
+        String[] parts = decode.split(":");
+        String username = parts[0];
+        User inDB = userRepository.findByUsername(username);
+        return ResponseEntity.ok(inDB);
+    }
 
-		return ResponseEntity.ok(inDB);
-	}
+
 }
