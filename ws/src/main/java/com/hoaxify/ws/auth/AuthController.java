8cae17e270d5ce93ce474b2a88a7d885053d0cce
İbrahim BAYRAMLI <ibrahimbayramli1902@gmail.com>
2023-04-13 04:17:04 +0300

    50 - Spring Security Sonrası Kimlik Doğrulama Başarılı Cevap (Spring)

--- ws/src/main/java/com/hoaxify/ws/auth/AuthController.java
+++ ws/src/main/java/com/hoaxify/ws/auth/AuthController.java
@@ -8,8 +8,6 @@ import com.hoaxify.ws.user.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
