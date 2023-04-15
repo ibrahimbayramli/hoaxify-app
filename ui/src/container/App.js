4915af3e54e2586429f0f6bfb95a14a1f033c180
İbrahim BAYRAMLI <ibrahim.bayramli@atmosware.turkcell.com.tr>
2023-04-15 04:14:54 +0300

    60 - React Router (React)

--- ui/src/container/App.js
+++ ui/src/container/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import LanguageSelector from '../components/LanguageSelector';
+import {HashRouter, Redirect, Route, Switch} from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/LoginPage";
+import UserSignupPage from "../pages/UserSignupPage";
 import UserPage from "../pages/UserPage";
 
 function App() {
     return (
-        <div className="container">
-            <UserPage/>
-            <LanguageSelector />
+        <div>
+            <HashRouter>
+                <Switch>
+                    <Route exact path={"/"} component={HomePage}/>
+                    <Route path={"/login"} component={LoginPage}/>
+                    <Route path={"/signup"} component={UserSignupPage}/>
+                    <Route path={"/user/:username"} component={UserPage}/>
+                    <Redirect to={"/"}/>
+                </Switch>
+            </HashRouter>
+            <LanguageSelector/>
         </div>
     );
 }
