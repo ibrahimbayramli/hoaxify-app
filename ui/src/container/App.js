315ee9a167e9662a3890d97d44c85c228fe590c7
İbrahim BAYRAMLI <ibrahim.bayramli@atmosware.turkcell.com.tr>
2023-04-15 04:47:42 +0300

    61 - TopBar (React)

--- ui/src/container/App.js
+++ ui/src/container/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import LanguageSelector from '../components/LanguageSelector';
-import {HashRouter, Redirect, Route, Switch} from "react-router-dom";
+import {HashRouter as Router, Redirect, Route, Switch} from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/LoginPage";
 import UserSignupPage from "../pages/UserSignupPage";
 import UserPage from "../pages/UserPage";
+import TopBar from "../components/TopBar";
 
 function App() {
     return (
         <div>
-            <HashRouter>
+            <Router>
+                <TopBar/>
                 <Switch>
                     <Route exact path={"/"} component={HomePage}/>
                     <Route path={"/login"} component={LoginPage}/>
@@ -17,7 +19,7 @@ function App() {
                     <Route path={"/user/:username"} component={UserPage}/>
                     <Redirect to={"/"}/>
                 </Switch>
-            </HashRouter>
+            </Router>
             <LanguageSelector/>
         </div>
     );
